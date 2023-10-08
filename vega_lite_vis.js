9a7c108b4358a1f677e519b0b130b800e3879e0e
Pei Ying Chong <112391587+peiyingchong@users.noreply.github.com>
2023-10-08 16:50:24 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "aboriginal_suicide.vg.json"; 
+vegaEmbed("#chloro_map", vg_1).then(function(result) {}).catch(console.error);
+
+var vg_2 = "leading_cause.vg.json"; 
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+        }).catch(console.error);
